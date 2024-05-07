3bd9e4d107326daa0ea7c56d15099f94a5a895b5
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-06 21:32:46 -0300

    Initial Commit

---
+++ Forecast/ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastApp.swift
+//  Forecast
+//
+//  Created by Antonio Vuono on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
